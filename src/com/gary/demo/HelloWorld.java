0d2036df125943eec2921a79f38cf0ae5626c261
GaryLi <827611572@qq.com>
2017-08-10 11:37:14 +0800

    123

--- src/com/gary/demo/HelloWorld.java
+++ src/com/gary/demo/HelloWorld.java
@@ -2,6 +2,6 @@ package com.gary.demo;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World~d");
+        System.out.println("Hello World~dddd");
     }
 }
